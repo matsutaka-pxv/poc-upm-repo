200d8f9f3b9b9b78b8bb8db4d69a242533fa2f93
matsutaka <matsutaka@pixiv.co.jp>
2026-04-09 18:10:28 +0900

    Remove "/-/all" generator files

--- publish-local.cmd
+++
@@ -1,3 +0,0 @@
-set REGISTRY_URL=http://localhost:4873
-call npm install tar || pause
-node generate-registry.js || pause
--- publish.cmd
+++
@@ -1,3 +0,0 @@
-set REGISTRY_URL=https://matsutaka-pxv.github.io/poc-upm-repo
-call npm install tar || pause
-node generate-registry.js || pause
